dd4462c9569bd1f3593c333aedf50e016b611525
plastic-plant <plastic-plant@patrick.nl>
2024-07-28 18:24:14 +0200

    Serialise enum values full lowercase for appsettings.json

---
+++ src/Application/Converters/LowercaseJsonStringEnumConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Tessa.Application.Converters
+{
+	public class LowercaseJsonStringEnumConverter : JsonConverterFactory
+	{
+		public override bool CanConvert(Type typeToConvert)
+		{
+			return typeToConvert.IsEnum;
+		}
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+		{
+			Type converterType = typeof(LowercaseEnumConverter<>).MakeGenericType(typeToConvert);
+			return (JsonConverter)Activator.CreateInstance(converterType);
+		}
+
+		private class LowercaseEnumConverter<T> : JsonConverter<T>
+			where T : struct, Enum
+		{
+			public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+				string value = reader.GetString();
+				if (Enum.TryParse(typeof(T), value, ignoreCase: true, out var result))
+				{
+					return (T)result;
+				}
+				throw new JsonException($"Unable to convert \"{value}\" to Enum \"{typeof(T)}\".");
+			}
+
+			public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+			{
+				string lowercaseValue = value.ToString().ToLower();
+				writer.WriteStringValue(lowercaseValue);
+			}
+		}
+	}
+}
---
+++ src/Application/Converters/ProviderConfigConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Tessa.Application.Enums;
+using Tessa.Application.Models.ProviderConfigs;
+
+namespace Tessa.Application.Converters;
+
+public class ProviderConfigConverter : JsonConverter<ProviderConfig>
+{
+
+    public override ProviderConfig? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var document = JsonDocument.ParseValue(ref reader);
+        var root = document.RootElement;
+        var json = root.GetRawText();
+
+        var provider = LlmProvider.Unknown;
+        var providerFound = root.GetProperty("provider").GetString();
+        Enum.TryParse(providerFound, ignoreCase: true, out provider);
+        return provider switch
+        {
+            LlmProvider.Jan => JsonSerializer.Deserialize<ProviderConfigJan>(json, options),
+            LlmProvider.Llama => JsonSerializer.Deserialize<ProviderConfigLlamaGguf>(json, options),
+            LlmProvider.LMStudio => JsonSerializer.Deserialize<ProviderConfigLMStudio>(json, options),
+            LlmProvider.OpenAI => JsonSerializer.Deserialize<ProviderConfigOpenAI>(json, options),
+            _ => JsonSerializer.Deserialize<ProviderConfig>(json, options)
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, ProviderConfig value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            var type = value.GetType();
+            JsonSerializer.Serialize(writer, value, type, options);
+        }
+    }
+}
